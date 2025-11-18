e55f39d7ff871fc1ed73d25d03b984eca6279ead
Lloyd R. Encallado <lloydencallado@gmail.com>
2025-11-18 22:47:44 +0800

    added product controller

---
+++ services/catalog/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' => Product::all()
+        ]);;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product): JsonResponse
+    {
+        return response()->json($product);
+    }
+}
