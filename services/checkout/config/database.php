2ec302d11ca2d07cb0c02670fec6affc4abac15e
Lloyd R. Encallado <lloydencallado@gmail.com>
2025-11-24 20:36:55 +0800

    added default redis scheme

--- services/checkout/config/database.php
+++ services/checkout/config/database.php
@@ -153,7 +153,7 @@ return [
         ],
 
         'default' => [
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'scheme' => env('REDIS_SCHEME', 'tls'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
