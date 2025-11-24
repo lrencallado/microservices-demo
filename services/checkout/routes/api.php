8da30151cc8a553e94038b79799e181b70c1e479
Lloyd R. Encallado <lloydencallado@gmail.com>
2025-11-24 18:20:23 +0800

    removed rewrite and added prefix on api route

--- services/checkout/routes/api.php
+++ services/checkout/routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Api\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/orders', [OrderController::class, 'store'])->middleware('throttle:10,1');
+Route::post('/checkout/orders', [OrderController::class, 'store'])->middleware('throttle:10,1');
