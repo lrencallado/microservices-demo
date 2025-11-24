83a27a721e8818f43853c754f08312b25119340d
Lloyd R. Encallado <lloydencallado@gmail.com>
2025-11-24 18:20:16 +0800

    removed rewrite and added prefix on api route

--- services/catalog/routes/api.php
+++ services/catalog/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('throttle:60,1')->group(function () {
-    Route::controller(ProductController::class)->prefix('products')->group(function () {
+    Route::controller(ProductController::class)->prefix('catalog/products')->group(function () {
         Route::get('/', 'index');
         Route::get('/{id}', 'show');
         Route::post('/{id}/decrement-stock', 'decrementStock');
